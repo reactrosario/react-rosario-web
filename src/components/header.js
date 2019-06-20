2b11851a71cb5385b2b31061e73bd1f2fa0a8072
Macarena Gómez <macarena.gomez@leniolabs.com>
2019-06-20 18:59:40 -0300

    ADD: header logo

--- src/components/header.js
+++ src/components/header.js
@@ -1,37 +1,55 @@
 import React from "react"
 import PropTypes from "prop-types"
+import Img from "gatsby-image"
 import styled from "styled-components"
-import { Link } from "gatsby"
+import { StaticQuery, graphql, Link } from "gatsby"
 
 
-const Header = ({ siteTitle }) => (
-  <header
-    style={{
-      background: `rebeccapurple`,
-      marginBottom: `1.45rem`,
-    }}
-  >
-    <div
-      style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-          }}
-        >
-          {siteTitle}
-        </Link>
-      </h1>
-    </div>
-  </header>
+const Header = () => (
+  <HeaderWrapper>
+    <HeaderContent>
+      <StaticQuery
+        query={graphql`
+          query {
+            placeholderImage: file(relativePath: { eq: "LogoRos.png" }) {
+              childImageSharp {
+                fluid(maxWidth: 300) {
+                  ...GatsbyImageSharpFluid
+                }
+              }
+            }
+          }
+        `}
+        render={data => <StyledImg fluid={data.placeholderImage.childImageSharp.fluid} />}
+      />      
+      <StyledLink to="/" />
+    </HeaderContent>
+  </HeaderWrapper>
 )
+
+const HeaderWrapper = styled.div`
+  margin: 3% 0;
+  background: linear-gradient(45deg, rgba(33,193,251,1) 0%, rgba(129,32,227,1) 100%);
+  width: 100%;
+  position: absolute;
+`;
+
+const HeaderContent = styled.div`
+  padding: 1% 3%;   
+  position: relative;
+  display: flex;
+  height: 80px;
+`
+
+const StyledLink = styled(Link)`
+  color: white;
+  text-decoration: none;
+`
+
+const StyledImg = styled(Img)`
+  height: 100%;
+  width: 12%;
+`
 
 Header.propTypes = {
   siteTitle: PropTypes.string,
