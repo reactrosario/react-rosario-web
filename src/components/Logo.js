787a705df2c07f8cb8ecaeaba787156b07362ca9
Octavio Bruzzesi <octa.bruzzesi@gmail.com>
2019-06-27 14:15:00 -0300

    ADD: logo to index page

---
+++ src/components/Logo.js
@@ -0,0 +1,48 @@
+import React from "react"
+import { StaticQuery, graphql } from "gatsby"
+import Img from "gatsby-image"
+import styled from "styled-components"
+
+/*
+ * This component is built using `gatsby-image` to automatically serve optimized
+ * images with lazy loading and reduced file sizes. The image is loaded using a
+ * `StaticQuery`, which allows us to load the image from directly within this
+ * component, rather than having to pass the image data down from pages.
+ *
+ * For more information, see the docs:
+ * - `gatsby-image`: https://gatsby.dev/gatsby-image
+ * - `StaticQuery`: https://gatsby.dev/staticquery
+ */
+
+const Logo = () => (
+  <StaticQuery
+    query={graphql`
+      query {
+        placeholderImage: file(relativePath: { eq: "logo.png" }) {
+          childImageSharp {
+            fluid(maxWidth: 900) {
+              ...GatsbyImageSharpFluid
+            }
+          }
+        }
+      }
+    `}
+    render={data =>
+      <StyledImg
+        fluid={data.placeholderImage.childImageSharp.fluid}
+        imgStyle={{
+          maxWidth: 900,
+          objectFit: 'contain',
+        }}
+      />
+    }
+  />
+)
+const StyledImg = styled(Img)`
+  height: 50%;
+  width: 50%;
+  margin: 0 5%;
+`
+
+export default Logo;
+
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,10 +2,12 @@ import React from "react"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+import Logo from '../components/Logo';
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
+    <Logo />
   </Layout>
 )
 
