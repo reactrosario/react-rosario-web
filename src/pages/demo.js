069a06c35684ea0ff8eea6c69043e1a96b5bbc30
Macarena Gómez <macarena.gomez@leniolabs.com>
2019-07-09 22:50:22 -0300

    FIX: move Button to components

--- src/pages/demo.js
+++ src/pages/demo.js
@@ -2,68 +2,42 @@ import React from "react"
 import styled from "styled-components"
 
 import Layout from "../components/layout"
+import Button from "../components/Button"
 import SEO from "../components/seo"
 
 const Demo = () => (
   <Layout>
     <SEO title="Demo" />
     <DemoWrapper>
-      <StyledButton 
+      <Button 
         background='Plum'
       > 
         First
-      </StyledButton> 
-      <StyledButton 
+      </Button> 
+      <Button 
         background='MediumPurple'
       >
         Second
-      </StyledButton>       
-      <StyledButton 
+      </Button>       
+      <OverrideButton 
         background='DarkMagenta'
       >        
         Third
-      </StyledButton>                  
+      </OverrideButton>                  
     </DemoWrapper>
   </Layout>
 )
+
+export default Demo
+
+const OverrideButton = styled(Button)`
+  border: 2px solid lime;
+`;
 
 const DemoWrapper = styled.div`
   display: flex;
   height: 100%;
   flex-direction: row;
-`
+`;
 
 const ReversedButton = props => <button {...props} children={props.children.split('').reverse()} />
-
-const StyledButton = styled.button`
-  height: 80px;
-  margin: 2%;
-  width: 100%;
-  color: white;
-  border-radius: 50px;
-  border: unset;
-  background-color: ${({ background }) => background || 'red'};
-
-  &:hover {
-    background-color: Indigo;
-    cursor: pointer;
-  }
-  
-  $.other-style{
-    border: 1px solid yellow; 
-  }
-
-  // & ~ & {
-  //   background: pink; // <StyledButton> as a sibling of <StyledButton>, but maybe not directly next to it
-  // }  
-
-  // & + & {
-  //   background: lime; // <StyledButton> next to <StyledButton>
-  // }  
-
-  .another-style & {
-    border: 2px solid lime; // <StyledButton> inside another element labeled ".something-else"
-  }  
-`
-
-export default Demo
