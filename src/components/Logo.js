a39bdbe6fe2ca5f0f7148c212119a15bd0315fa9
Octavio Bruzzesi <octa.bruzzesi@gmail.com>
2019-07-23 12:52:39 -0300

    Wip

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -1,7 +1,8 @@
 import React from "react"
 import { StaticQuery, graphql } from "gatsby"
 import Img from "gatsby-image"
-import styled from "styled-components"
+import styled, { keyframes } from "styled-components"
+import LogoImage from '../images/logo.svg';
 
 /*
  * This component is built using `gatsby-image` to automatically serve optimized
@@ -15,41 +16,73 @@ import styled from "styled-components"
  */
 
 const Logo = () => (
-  <LogoWrapper>
-    <StaticQuery
-      query={graphql`
-        query {
-          placeholderImage: file(relativePath: { eq: "logo.png" }) {
-            childImageSharp {
-              fluid(maxWidth: 900) {
-                ...GatsbyImageSharpFluid
-              }
-            }
-          }
-        }
-      `}
-      render={data =>
-        <StyledImg
-          fluid={data.placeholderImage.childImageSharp.fluid}
-          imgStyle={{
-            maxWidth: 900,
-            objectFit: 'contain',
-          }}
-        />
-      }
-    />
-  </LogoWrapper>
+  <StyledLogo />
+  // <StaticQuery
+  //   query={graphql`
+  //     query {
+  //       placeholderImage: file(relativePath: { eq: "logo.png" }) {
+  //         childImageSharp {
+  //           fluid(maxWidth: 900) {
+  //             ...GatsbyImageSharpFluid
+  //           }
+  //         }
+  //       }
+  //     }
+  //   `}
+  //   render={data =>
+      // <StyledImg
+      //   fluid={data.placeholderImage.childImageSharp.fluid}
+      //   imgStyle={{
+      //     maxWidth: 900,
+      //     objectFit: 'contain',
+      //   }}
+      // />
+  //   }
+  // />
 )
 
-const LogoWrapper = styled.div`
-  display: flex;
-  justify-content: center;
+const wiggle = keyframes`
+  20%  { transform: translateX(4px);  }
+  40%  { transform: translateX(-4px); }
+  60%  { transform: translateX(2px);  }
+  80%  { transform: translateX(-1px); }
+  100% { transform: translateX(0);    }
+`;
+
+const fade = keyframes`
+0% {
+fill:#61DAFB;
+}
+50% {
+ fill:black;
+}
+100%{
+  fill:#61DAFB;
+}
+`;
+
+const coolBoxKeyframes = keyframes`
+  0% {
+    height: 0px;
+    background: green;
+  }
+  100% {
+    height: 200px;
+    background: blue;
+  }
 `
 
-const StyledImg = styled(Img)`
-  height: 50%;
-  width: 50%;
-  margin: 0 5%;
+const StyledLogo = styled(LogoImage)`
+  margin-top: -395px;
+  .fil0, .fil1, .fil2, .fil3 {
+    transition: 1s;
+  }
+  .fil0:hover {
+    transform: translateX(500px);
+  }
+  .fil1:hover {
+    transform: translateY(500px);
+  }
 `
 
 export default Logo;
