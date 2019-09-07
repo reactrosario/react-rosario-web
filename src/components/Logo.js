20bb09545944ccb4df43dfbde039722ffb8c5d2f
Octa Bruzzesi <octa.bruzzesi>
2019-09-07 12:31:52 -0300

    Fix logo styles

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -4,14 +4,17 @@ import LogoImage from '../images/logo.svg';
 
 const Logo = () => {
   return (
-    <Fragment>
+    <Container>
       <StyledLogo />
-    </Fragment>
+    </Container>
   );
 }
 
+const Container = styled.div`
+  max-width: 500px;
+`
+
 const StyledLogo = styled(LogoImage)`
-  width: 50%;
   .fil0, .fil1, .fil2, .fil3 {
     transition: 0.5s;
   }
