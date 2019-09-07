2d0b12851d8e721b7c6462491dee7e36b868f1f9
Juan Helbert <helbert.juan@gmail.com>
2019-09-07 10:54:16 -0300

    First Commit

---
+++ src/pages/contributors.js
@@ -0,0 +1,14 @@
+import React from "react"
+
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+import Logo from '../components/Logo';
+
+const IndexPage = () => (
+  <Layout>
+    <SEO title="Home" />
+    Page test
+  </Layout>
+)
+
+export default IndexPage
